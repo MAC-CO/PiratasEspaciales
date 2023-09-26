d350b11867f06950c484a01fc932aa033468ab66
David Mieles <mieles099@gmail.com>
2023-09-26 11:41:26 -0500

    Ragdoll
    En la escena Mieles se realizó el ragdoll del personaje principal

---
+++ Assets/Scripts/RagDoll.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagDoll : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    private Rigidbody[] rigidbodies;
+
+    void Start()
+    {
+        rigidbodies = transform.GetComponentsInChildren<Rigidbody>();
+        SetEnabled(false);
+    }
+
+    void SetEnabled(bool enabled)
+    {
+        bool isKinematic = !enabled;
+        foreach (Rigidbody rigidbody in rigidbodies)
+        {
+            rigidbody.isKinematic = isKinematic;
+        }
+
+        animator.enabled = !enabled;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SetEnabled(true);
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        { 
+            SetEnabled(false); 
+        }
+    }
+
+}
