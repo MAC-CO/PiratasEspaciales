45cefb5b46c4135cecf14e24af1538f22b82f7aa
SJKADGBJKASGDDJSA <143735609+SJKADGBJKASGDDJSA@users.noreply.github.com>
2023-09-26 11:48:17 -0500

    mecanicas de apuntado
    script para markos

--- Assets/Scripts/Movimiento/Dmov2.cs
+++ Assets/Scripts/Movimiento/Dmov2.cs
@@ -8,6 +8,10 @@ public class Dmov2 : MonoBehaviour
 {
     public StarterAssets.ThirdPersonController controlador;
     public InputActionProperty agacharse;
+    public InputActionProperty Apuntar;
+    public InputActionProperty Disparar;
+    public float Disparo;
+    public float Apuntado;
 
     public float estaAgachado;
 
@@ -19,8 +23,39 @@ public class Dmov2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MecanicasDeDisparo();
+        MecanicasDeApuntado();
+        
         estaAgachado = agacharse.action.ReadValue<float>();
 
         controlador.Agachado(estaAgachado>0.5f);
     }
+
+    public void MecanicasDeDisparo()
+    {
+        
+        Disparo = Disparar.action.ReadValue<float>();
+        if(Disparo >= 1)
+        {
+        Debug.Log("el sujeto ha disparado");    
+        }
+        
+    }
+
+    public void MecanicasDeApuntado()
+    {
+        Apuntado = Apuntar.action.ReadValue<float>();
+
+        if(Apuntado >= 1)
+        {
+        Debug.Log("el sujeto esta apuntando");  
+        }else{
+        Debug.Log("No esta apuntando");   
+        }
+        
+        
+    }
+
+    
+
 }
