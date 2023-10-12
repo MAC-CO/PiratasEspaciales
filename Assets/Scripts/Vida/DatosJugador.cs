948f9521c5f3568feffebb4da6457e39800e7a5d
David Mieles <mieles099@gmail.com>
2023-10-12 11:57:26 -0500

    Respawn

--- Assets/Scripts/Vida/DatosJugador.cs
+++ Assets/Scripts/Vida/DatosJugador.cs
@@ -7,6 +7,16 @@ public class DatosJugador : MonoBehaviour
 {
     public int vidaPlayer;
     public Slider VidaVisual;
+    public int maxHealth;
+    public List<Transform> respawnPoints = new List<Transform>();
+    public GameObject player;
+
+
+
+    public void Start()
+    {
+        vidaPlayer = maxHealth;
+    }
 
     private void Update()
     {
@@ -16,6 +26,45 @@ public class DatosJugador : MonoBehaviour
         if (vidaPlayer <=0 )
         {
             Debug.Log("jaja, te mataron");
+
+            RespawnPlayer();
+
+            vidaPlayer = maxHealth;
+        }
+
+    }
+
+    public void ActualizarInterfaz()
+    {
+        VidaVisual.value = vidaPlayer;
+    }
+
+
+    public void RespawnPlayer()
+    {
+        Debug.Log("Se activa el Respawn");
+        if (respawnPoints.Count > 0)
+        {
+            int randomIndex = Random.Range(0, respawnPoints.Count);
+            Transform selectedRespawnPoint = respawnPoints[randomIndex];
+
+            // Mueve al jugador al punto de respawn seleccionado
+            GameObject player = GameObject.FindGameObjectWithTag("Player"); // Ajusta la etiqueta "Player" según tu juego
+            player.GetComponent<CharacterController>().Move(selectedRespawnPoint.position - transform.position);
+            transform.position = selectedRespawnPoint.position;
+            Debug.Log("DEberia respawmear");
+        }
+        else
+        {
+            Debug.LogWarning("No hay puntos de respawn disponibles.");
         }
     }
+
+
+
+
+
+
+
+
 }
