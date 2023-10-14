30340a23f245b15ef3e152255cb7d4e5e9b422e6
SJKADGBJKASGDDJSA <drakonitetv@gmail.com>
2023-10-14 02:25:33 -0500

    update

--- Assets/Scripts/Rooms/Launcher.cs
+++ Assets/Scripts/Rooms/Launcher.cs
@@ -8,7 +8,11 @@ using System.Linq;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
-	void Start()
+    [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] TMP_Text errorText;
+    [SerializeField] TMP_Text roomNameText;
+	
+    void Start()
     {
         Debug.Log("Connecting to Master");
         PhotonNetwork.ConnectUsingSettings();
@@ -16,12 +20,47 @@ public class Launcher : MonoBehaviourPunCallbacks
 
     public override void OnConnectedToMaster()
     {
+        
         Debug.Log("Connected to Master");
         PhotonNetwork.JoinLobby();
     }
 
     public override void OnJoinedLobby()
     {
+        MenuManager.Instance.OpenMenu("title");
         Debug.Log("Joined Lobby");
+    }
+
+    public void CreateRoom()
+    {
+        if(string.IsNullOrEmpty(roomNameInputField.text))
+        {
+            return;
+        }
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+        MenuManager.Instance.OpenMenu("loading");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        MenuManager.Instance.OpenMenu("room");
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        errorText.text = "Room Creation Failed: " + message;
+        MenuManager.Instance.OpenMenu("error");
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.Instance.OpenMenu("loading");
+    }
+
+    public override void OnLeftRoom()
+    {
+         MenuManager.Instance.OpenMenu("title");
     }
 }
