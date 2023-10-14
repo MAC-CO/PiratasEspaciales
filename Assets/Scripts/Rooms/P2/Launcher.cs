d9dda8c1d85d2e71adf7da0014d4be7eae78edc2
SJKADGBJKASGDDJSA <drakonitetv@gmail.com>
2023-10-14 03:27:18 -0500

    HabemusNetWorking

--- Assets/Scripts/Rooms/P2/Launcher.cs
+++ Assets/Scripts/Rooms/P2/Launcher.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 using Photon.Pun;
 using TMPro;
 using Photon.Realtime;
-using System.Linq;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    public static Launcher Instance;
+
     [SerializeField] TMP_InputField roomNameInputField;
     [SerializeField] TMP_Text errorText;
     [SerializeField] TMP_Text roomNameText;
-	
+	[SerializeField] Transform roomListContent;
+    [SerializeField] GameObject roomListItemPrefab;
+
     void Start()
     {
         Debug.Log("Connecting to Master");
@@ -59,8 +62,29 @@ public class Launcher : MonoBehaviourPunCallbacks
         MenuManager.Instance.OpenMenu("loading");
     }
 
+    public void JoinRoom(RoomInfo info)
+	{
+		PhotonNetwork.JoinRoom(info.Name);
+		MenuManager.Instance.OpenMenu("loading");
+	}
+
     public override void OnLeftRoom()
     {
          MenuManager.Instance.OpenMenu("title");
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach(Transform trans in roomListContent)
+		{
+			Destroy(trans.gameObject);
+		}
+
+        for(int i = 0; i < roomList.Count; i++)
+		{
+			if(roomList[i].RemovedFromList)
+				continue;
+			Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
+		}
     }
 }
